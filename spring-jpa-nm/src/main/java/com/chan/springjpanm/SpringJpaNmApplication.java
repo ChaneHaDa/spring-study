fc0de6545a60ed15e5458dd58875662df671dbc3
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-07 23:29:07 +0900

    initial commit

---
+++ spring-jpa-nm/src/main/java/com/chan/springjpanm/SpringJpaNmApplication.java
@@ -0,0 +1,13 @@
+package com.chan.springjpanm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaNmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaNmApplication.class, args);
+    }
+
+}
---
+++ spring-jpa-nm/src/test/java/com/chan/springjpanm/SpringJpaNmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.springjpanm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaNmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
