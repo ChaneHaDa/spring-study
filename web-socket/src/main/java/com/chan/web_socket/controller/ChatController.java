d0cdc19fcf09021b0dad27f15f95301023bb9ee5
ChaneHaDa <chane.ha.da@gmail.com>
2025-07-31 23:15:32 +0900

    연결 종료 처리 추가

--- web-socket/src/main/java/com/chan/web_socket/controller/ChatController.java
+++ web-socket/src/main/java/com/chan/web_socket/controller/ChatController.java
@@ -4,7 +4,10 @@ import com.chan.web_socket.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
+
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,8 +16,10 @@ public class ChatController {
     private final SimpMessageSendingOperations messagingTemplate;
 
     @MessageMapping("/chat/message")
-    public void message(ChatMessage message) {
+    public void message(ChatMessage message, StompHeaderAccessor headerAccessor) {
         if (ChatMessage.MessageType.JOIN.equals(message.getType())) {
+            Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", message.getSender());
+            Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("roomId", message.getRoomId());
             message.setContent(message.getSender() + "님이 입장하셨습니다.");
         }
         messagingTemplate.convertAndSend("/topic/room/" + message.getRoomId(), message);
