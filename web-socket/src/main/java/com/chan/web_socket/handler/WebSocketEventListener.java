a8e01b16ba8d29bd1da6a565ab69b0f20ea60bc2
ChaneHaDa <chane.ha.da@gmail.com>
2025-07-31 23:15:07 +0900

    연결 종료 이벤트 리스너 추가

---
+++ web-socket/src/main/java/com/chan/web_socket/handler/WebSocketEventListener.java
@@ -0,0 +1,40 @@
+package com.chan.web_socket.handler;
+
+import com.chan.web_socket.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
+
+        if (Objects.nonNull(username) && Objects.nonNull(roomId)) {
+            log.info("User Disconnected : " + username);
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+            chatMessage.setRoomId(roomId);
+            chatMessage.setContent(username + "님이 퇴장하셨습니다.");
+
+            messagingTemplate.convertAndSend("/topic/room/" + roomId, chatMessage);
+        }
+    }
+}
